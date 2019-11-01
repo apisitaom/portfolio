44177587612e23aa8dbb9c5d634e82116e00f45f
apisitprompha <apisitprompha@gmail.com>
2019-11-02 04:25:36 +0700

    เสร็จ result

--- backend/src/routes/result.js
+++ backend/src/routes/result.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
+const result = require('../services/result');
 
 router.get('/', (req, res) => {res.json({ info : `result route`})});
+router.get('/lists', result.lists);
+
+router.post('/add', result.add);
+router.post('/edit/:id', result.edit);
+
+router.delete('/deleted/:id', result.deleted);
 
 module.exports = router;
--- backend/src/services/result.js
+++ backend/src/services/result.js
@@ -3,3 +3,49 @@ const responces = require('../libs/responces');
 const success = require('../libs/success');
 const errors = require('../libs/error');
 const moment = require('moment');
+
+async function add(req, res, next) {
+    const { title, result } = req.body;
+    const sql = `insert into result (title, result) values ($1, $2)`
+    const values = [ title, result ]
+    try {
+        db.query(sql, values);
+        return responces.success(res, success.success);
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function edit(req, res, next) {
+    const { title, result } = req.body;
+    const sql = `update result set title = $1, result = $2 where resultid = $3`
+    const values = [ title, result, req.params.id ]
+    try {
+        db.query(sql, values);
+        return responces.success(res, success.updated);
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+async function lists(req, res, next) {
+    const sql = `select title, result from result order by createdate asc`
+    try {
+        const { rows } = await db.query(sql);
+        return responces.success(res, success.success, rows);
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }   
+}
+
+async function deleted(req, res, next) {
+    const sql = `delete from result where resultid = $1`
+    try {
+        db.query(sql, [req.params.id]);
+        return responces.success(res, success.deleted);
+    } catch (error) {
+        return responces.error(res, errors.server);
+    }
+}
+
+module.exports = { add, edit, lists, deleted }
