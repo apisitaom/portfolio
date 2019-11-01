35937a7c14a63961c143de4cbfd005be0042fc3b
apisitprompha <apisitprompha@gmail.com>
2019-11-02 04:36:31 +0700

    ลบ comment imageupload

--- backend/src/libs/imageupload.js
+++ backend/src/libs/imageupload.js
@@ -1,14 +1,6 @@
 const multer = require('multer');
 const path = require('path');
 
-// const storage = multer.diskStorage({
-//   destination: ('./public/images/'),
-//   filename: function (req, file, cb) {
-//     cb(null, file.fieldname + '-' + new Date().getTime().toString() + path.extname(file.originalname));
-//     const text = file.fieldname + '-' + new Date().getTime().toString() + path.extname(file.originalname);
-//     req.picture = text;
-//   }
-// });
 const storage = multer.diskStorage({
     destination: ('./public/images/'),
     filename: function(req, file, cb) {
@@ -31,18 +23,3 @@ const upload = multer({
 }).array('picture', 10);
 
 module.exports = { upload };
-// const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 2 * 1024 * 1024 // 2 MB
-//   },
-//   fileFilter: function (req, file, callback) {
-//     var ext = path.extname(file.originalname);
-//     if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-//         return callback(new Error('Only images are allowed'));
-//     }
-//     callback(null, true);
-// }
-// }).array('picture', 10);
-
-// module.exports = { upload }
