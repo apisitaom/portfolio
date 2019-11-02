b8bb4b1bb4ff6bbdb8c66a246875b300af946842
apisitprompha <apisitprompha@gmail.com>
2019-11-02 19:34:48 +0700

    สร้าง views Page

---
+++ frontend/src/views/Pages/owner/Lists.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Lists extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Lists</h1>
+            </div>
+        )
+    }
+}
