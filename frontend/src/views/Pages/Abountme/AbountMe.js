0a04615fb29288f9adeea35fb7b4b14bcecad80d
apisitprompha <apisitprompha@gmail.com>
2019-11-17 14:37:42 +0700

    เสร็จเเบบฟอร์ม resume

--- frontend/src/views/Pages/Abountme/AbountMe.js
+++ frontend/src/views/Pages/Abountme/AbountMe.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Divider } from 'antd'
 import TabFrom from '../../../froms/nav/TabFrom' 
 export default class AbountMe extends Component {
     render() {
         return (
-            <Row>
-                <Col>
-                    <TabFrom />
-                </Col>
-            </Row>
+            <div>
+                <Row>
+                    <Col>
+                        <TabFrom />
+                    </Col>
+                </Row>
+                <Row style={{paddingTop: '1.5%'}}>
+                    <Col offset={6} span={8}><p style={{fontSize: '450%'}}>ABOUT ME</p></Col>
+                    <Col offset={6} span={8}> 
+                    <Divider />                       
+                        <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    </Col>
+                    <Col offset={1} span={3} style={{paddingTop: '1.5%'}}>
+                        <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
--- frontend/src/views/Pages/Resume/Resume.js
+++ frontend/src/views/Pages/Resume/Resume.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Divider, Tag, Icon } from 'antd'
 import TabFrom from '../../../froms/nav/TabFrom'
 export default class Resume extends Component {
     render() {
         return (
-            <Row>
-                <Col>
-                    <TabFrom />
-                </Col>
-            </Row>
+            <div>
+                <Row>
+                    <Col>
+                        <TabFrom />
+                    </Col>
+                </Row>
+                <Row style={{paddingTop: '1.5%'}}>
+                    <Col offset={6} span={8}><p style={{fontSize: '450%'}}>RESUME</p></Col>
+                    <Col offset={6} span={8}>                        
+                        <Tag color="gold" style={{fontSize: '150%'}}>Professional ​info​​</Tag>
+                        <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    <Divider />
+                        <Tag color="lime" style={{fontSize: '150%'}}>Work​ experience​</Tag>
+                        <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    <Divider />
+                        <Tag color="green" style={{fontSize: '150%'}}>Education</Tag>
+                        <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    <Divider />
+                        <Tag color="cyan" style={{fontSize: '150%'}}>Activity</Tag>
+                        <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    </Col>
+
+
+                    <Col offset={1} span={3} style={{paddingTop: '1.5%'}}>
+                        <Icon style={{fontSize:'300%'}} type="unlock" /> 
+                        <Tag color="blue" style={{fontSize: '80%'}}>Skills</Tag>                       
+                            <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    <Divider />
+                        <Icon style={{fontSize:'300%'}} type="global" /> 
+                        <Tag color="geekblue" style={{fontSize: '80%'}}>Language</Tag>                                              
+                            <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    <Divider />
+                        <Icon style={{fontSize:'300%'}} type="star" />
+                        <Tag color="purple" style={{fontSize: '80%'}}>Language</Tag>                                                                     
+                            <p>"NYC" and "New York, New York" redirect here.
+                            For other uses, see New York City (disambiguation); 
+                            NYC (disambiguation); and New York, New York (disambiguation).
+                        </p>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
