f552467e62a326b461427eb5ca90eda3131d3a37
apisitprompha <apisitprompha@gmail.com>
2019-11-18 04:18:54 +0000

    footer owner

--- frontend/src/views/Pages/Owner/Owner.js
+++ frontend/src/views/Pages/Owner/Owner.js
@@ -23,7 +23,7 @@ export default class Owner extends Component {
                         <OwnerListFrom />
                     </Col>
                 </Row>
-                <Row style={{paddingTop: '160px'}}>
+                <Row style={{paddingTop: '0.5%'}}>
                     <Col span={24}>
                         <OwnerDetailFrom />
                     </Col>
